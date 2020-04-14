520c5d84f00d1cbbc70c3170f478d3cd0b7fbea6
Renato <renato145@hotmail.com>
2020-04-15 02:31:23 +0930

    Updates

---
+++ precache-manifest.ed7acc68f459293eef5c70d5da0f4440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aff337db4e7996ebc5444288378db63",
+    "url": "/gcn-js/index.html"
+  },
+  {
+    "revision": "51f344481d4755646906",
+    "url": "/gcn-js/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "293f13dcd8c9192c4fde",
+    "url": "/gcn-js/static/css/main.5d06d5fe.chunk.css"
+  },
+  {
+    "revision": "51f344481d4755646906",
+    "url": "/gcn-js/static/js/2.6514e6f5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gcn-js/static/js/2.6514e6f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293f13dcd8c9192c4fde",
+    "url": "/gcn-js/static/js/main.a9f759c3.chunk.js"
+  },
+  {
+    "revision": "6900edf575c81381a0e8",
+    "url": "/gcn-js/static/js/runtime-main.244a51ed.js"
+  }
+]);
